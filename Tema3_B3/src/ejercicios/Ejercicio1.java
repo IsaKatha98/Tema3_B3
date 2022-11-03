88ce8f5b2b63af919e878a73b671d4fe60458ec7
ikloerzer <ikloerzer@106-14.info2>
2022-11-03 14:43:05 +0100

    C1

---
+++ Tema3_B3/src/ejercicios/Ejercicio1.java
@@ -0,0 +1,29 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		//Declaramos las variables.
+		
+		int num; //Variable introducida por el usuario.
+		int contador=0;
+		
+		/*Creamos un escáner para poder leer los datos que introduzca el usuario por
+		la consola. */
+													
+		Scanner sc= new Scanner (System.in);
+													
+		//Le pediremos al usuario que introduzca los datos y los guardaremos en la variable.
+						
+		System.out.print("Introduzca un número y pulse Intro: ");
+		num=sc.nextInt();
+		
+		//Instrucciones.
+		
+		
+	}
+
+}
